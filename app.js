1381ffe6b6d4aaa8b474a37daa4904b0e19839e8
Carlos_Z <carlosmariozambrano2015@gmail.com>
2021-10-05 23:29:45 -0500

    header completo

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-console.log('hello world');
+const menuButton = document.querySelector('.nav__button-menu');
+const mobileMenu = document.querySelector('.h__mobile-menu');
 
+menuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('show-menu');
+});
